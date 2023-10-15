03bcede443fe9616920dae9b8a4c1978ec40d777
Victor Jefferson <vrjefferson33@gmail.com>
2023-10-15 11:20:53 -0700

    saved changes

--- app/nc_tutorials/settings.py
+++ app/nc_tutorials/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = 'django-insecure-(j-#aqq@vs$3l#0(031z4_h(_*pa&ye#dt&x69_@(&a6(=+fpg
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'ec2-18-119-83-145.us-east-2.compute.amazonaws.com','0.0.0.0', 'localhost', '127.0.0.1'
-    ]
+    'ec2-18-119-83-145.us-east-2.compute.amazonaws.com', '0.0.0.0', 'localhost', '127.0.0.1'
+]
 
 
 # Application definition
